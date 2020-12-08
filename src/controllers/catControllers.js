a1d307cfc131c668b3dcc3ffce941025c4c77614
Paxnamra <bialyfeniks4@gmail.com>
2020-12-08 23:56:30 +0100

    switched res.send to res.json

--- src/controllers/catControllers.js
+++ src/controllers/catControllers.js
@@ -12,47 +12,47 @@ const addNewCat = (req, res) => {
     let newCat = new Cat(req.body);
 
     newCat.save()
-        .then(cat => res.json(cat))
-        .catch(err => res.status(201).send(err));
+        .then(cat => res.status(201).json(cat))
+        .catch(err => res.json(err));
 }
 
 const getAllCats = (req, res) => {
     Cat.find({})
         .then(cat => {
             try {
-                res.status(200).send({data: cat})
+                res.status(200).json({data: cat})
                 } catch (err) {
-            }}).catch(err => res.send(err));
+            }}).catch(err => res.json(err));
 }
 
 const findCatByID = (req, res) => {
     Cat.findById(req.params.catID)
         .then(cat => {
             try {
-                res.status(200).send({data: cat})
+                res.status(200).json({data: cat})
                 } catch (err) {
             }})
-        .catch(err => res.send(err));
+        .catch(err => res.json(err));
 }
 
 const updateCat = (req, res) => {
     Cat.findOneAndUpdate({_id: req.params.catID}, req.body, {new: true, useFindAndModify: false})
         .then(cat => {
             try {
-                res.status(202).send({data: cat})
+                res.status(202).json({data: cat})
                 } catch (err) {
             }})
-        .catch(err => res.send(err));
+        .catch(err => res.json(err));
 }
 
 const deleteCat = (req, res) => {
     Cat.remove({_id: req.params.catID})
         .then(cat => {
             try {
-                res.status(204).send({message: 'Successfully deleted a cat'})
+                res.status(204).json({message: 'Successfully deleted a cat'})
                 } catch (err) {
             }})
-        .catch(err => res.send(err));
+        .catch(err => res.json(err));
 }
 
 export {addNewCat, getAllCats, findCatByID, updateCat, deleteCat};
